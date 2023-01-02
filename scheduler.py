f53dd4dd232a0dff99ccf5787f71d9cff78c28dc
Alejandro Guayaquil <alejandro2206@gmail.com>
2023-01-01 17:38:49 -0800

    .

--- scheduler.py
+++ scheduler.py
@@ -1,11 +1,16 @@
+import argparse
 import time
 import os
 
+TEXLIVE_EXECUTABLE_PATH = "C:\\texlive\\2022\\bin\\win32\\xelatex.exe"
+# MIKTEX_EXECUTABLE_PATH = "C:\\Users\\aleja\\AppData\\Local\\Programs\\MiKTeX\\miktex\\bin\\x64\\xelatex.exe"
+MIKTEX_EXECUTABLE_PATH = "C:\\Users\\aleja\\AppData\\Local\\Programs\\MiKTeX\\miktex\\bin\\x64\\pdflatex.exe"
 show_execution_time = True
 
-def ExecuteLatexAndGetTime():
+def ExecuteLatexAndGetTime(latex_executable_path:str, latex_command_options):
+    command_to_run = latex_executable_path + latex_command_options + " main.tex > log.txt"
     t1 = time.time()
-    os.system("xelatex.exe --quiet main.tex")
+    os.system(command_to_run)
     t2 = time.time()
     return (t2 - t1)
 
@@ -23,7 +28,22 @@ def show(key):
             show_execution_time ^= 1
 
 if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--latex_executable", default="miktex")
+    console_arguments = parser.parse_args()
+    latex_executable = None
+    latex_command_options = None
+    if   (console_arguments.latex_executable == "miktex"):
+        latex_executable = MIKTEX_EXECUTABLE_PATH
+        latex_command_options = " --quiet"
+    elif (console_arguments.latex_executable == "texlive"):
+        latex_executable = TEXLIVE_EXECUTABLE_PATH
+        latex_command_options = ""
+    else:
+        print("Unknown latex executable. Aborting.")
+        exit()
+    print("Using : {}".format(latex_executable))
     while (True):
-        last_time = ExecuteLatexAndGetTime()
+        last_time = ExecuteLatexAndGetTime(latex_executable, latex_command_options)
         if (show_execution_time):
             print(last_time)
